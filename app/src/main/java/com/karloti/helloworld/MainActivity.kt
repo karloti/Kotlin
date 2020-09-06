a864ad4d232b88ae19ed0a9fa39c69dcb8af4731
Karloti <skarloti@gmail.com>
2020-09-06 18:15:04 +0300

    Android Kotlin Fundamentals 01.2: Anatomy of Basic Android Project
    Challenge:
    
    Add a second button to the app labeled "Count Up" that appears just below the Roll button. When tapped, the Count Up button should get the current value of the result text view, add 1 to it, and update the text view. Make sure you handle these edge cases:
    
    If the result text view does not yet contain a number (that is, if the text view still has the default "Hello World" string), set the result text to 1.
    If the number is already 6, do nothing.
    
    Signed-off-by: Karloti <skarloti@gmail.com>

--- app/src/main/java/com/karloti/helloworld/MainActivity.kt
+++ app/src/main/java/com/karloti/helloworld/MainActivity.kt
@@ -1,11 +1,24 @@
 package com.karloti.helloworld
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    private var number: Int = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun incText(view: View) {
+        if (number != 0) number = resultText.text.toString().toInt()
+        if (number < 6) number++
+        resultText.text = number.toString()
+    }
+
+    fun rollDice(view: View) {
+        resultText.text = (1..6).random().toString()
     }
 }
