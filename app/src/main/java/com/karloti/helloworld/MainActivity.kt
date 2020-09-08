56dafbe119999d57b3a8831f16e18e2eec7189b3
Karloti <skarloti@gmail.com>
2020-09-08 16:05:13 +0300

    Android Kotlin Fundamentals 01.2: Anatomy of Basic Android Project
    Challenge:
    
    Add a second button to the app labeled "Count Up" that appears just below the Roll button. When tapped, the Count Up button should get the current value of the result text view, add 1 to it, and update the text view. Make sure you handle these edge cases:
    
    If the result text view does not yet contain a number (that is, if the text view still has the default "Hello World" string), set the result text to 1.
    If the number is already 6, do nothing.
    
    Signed-off-by: Karloti <skarloti@gmail.com>

--- app/src/main/java/com/karloti/helloworld/MainActivity.kt
+++ app/src/main/java/com/karloti/helloworld/MainActivity.kt
@@ -1,24 +1,71 @@
 package com.karloti.helloworld
 
 import android.os.Bundle
-import android.view.View
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import kotlinx.android.synthetic.main.activity_main.*
+import com.karloti.helloworld.databinding.ActivityMainBinding
+
+/**
+ * DiceRoller demonstrates simple interactivity in an Android app.
+ * It contains one button that updates an image view with a dice
+ * vector image with a random value between 1 and 6.
+ */
 
 class MainActivity : AppCompatActivity() {
-    private var number: Int = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+
+        /** setContentView(R.layout.activity_main) */
+        setContentView(binding.root)
+
+        // Get the Button view from the layout and assign a click
+        // listener to it.
+
+        /**
+         * val rollButton: Button = findViewById(R.id.roll_button)
+         * rollButton.setOnClickListener { rollDice() }
+         */
+        binding.rollButton.setOnClickListener { rollDice() }
+
+        /**
+         * val countButton: Button = findViewById(R.id.countup_button)
+         * countButton.setOnClickListener { countUp() }
+         */
+        binding.countupButton.setOnClickListener { countUp() }
     }
 
-    fun incText(view: View) {
-        if (number != 0) number = resultText.text.toString().toInt()
-        if (number < 6) number++
-        resultText.text = number.toString()
+    /**
+     * Click listener for the Roll button.
+     */
+    private fun rollDice() {
+        val randomInt = (1..6).random()
+
+        val resultText: TextView = findViewById(R.id.result_text)
+        resultText.text = randomInt.toString()
     }
 
-    fun rollDice(view: View) {
-        resultText.text = (1..6).random().toString()
+    /**
+     * Click listener for the countUp button.
+     */
+    private fun countUp() {
+        val resultText: TextView = findViewById(R.id.result_text)
+
+        // If text is the default "Hello World!" set that text to 1.
+        if (resultText.text == "Hello World!") {
+            resultText.text = "1"
+//            Toast.makeText(this, "If text is the default \"Hello World!\" set that text to 1.", Toast.LENGTH_SHORT).show()
+        } else {
+            // Otherwise, increment the number up to 6.
+            // The text value in resultText.text is an instance of the CharSequence class;
+            // it needs to be converted to a String object before it can be converted to an int.
+            var resultInt = resultText.text.toString().toInt()
+
+            if (resultInt < 6) {
+                resultInt++
+                resultText.text = resultInt.toString()
+            }
+        }
     }
 }
